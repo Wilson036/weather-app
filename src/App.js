e553782a888b67daf4f424a71c07b879e2d5bebe
wilson kao <wilkao036@gmail.com>
2021-04-01 02:02:31 +0800

    use theme provider to set theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
 import { ReactComponent as DayCloudy } from './images/day-cloudy.svg';
 import { ReactComponent as RainIcon } from './images/rain.svg';
 import { ReactComponent as AirFlowIcon } from './images/airFlow.svg';
 import { ReactComponent as RefreshIcon } from './images/refresh.svg';
 
 const Container = styled.div`
-  background-color: #ededed;
+  background-color: ${({ theme }) => theme.backgroundColor};
   height: 100%;
   display: flex;
   align-items: center;
@@ -16,21 +16,21 @@ const Container = styled.div`
 const WeatherCard = styled.div`
   position: relative;
   min-width: 360px;
-  box-shadow: 0 1px 3px 0 #999999;
+  box-shadow: ${({ theme }) => theme.boxShadow};
   box-sizing: border-box;
   padding: 30px 15px;
-  background-color: #f9f9f9;
+  background-color: ${({ theme }) => theme.foregroundColor}; ;
 `;
 
 const Location = styled.div`
   font-size: 28px;
-  color: #212121;
+  color: ${({ theme }) => theme.textColor};
   margin-bottom: 20px;
 `;
 
 const Description = styled.div`
   font-size: 16px;
-  color: #828282;
+  color: ${({ theme }) => theme.titleColor};
   margin-bottom: 30px;
 `;
 
@@ -42,7 +42,7 @@ const CurrentWeather = styled.div`
 `;
 
 const Temperature = styled.div`
-  color: #757575;
+  color: ${({ theme }) => theme.temperatureColor};
   font-size: 96px;
   font-weight: 300;
   display: flex;
@@ -53,13 +53,7 @@ const Celsius = styled.div`
   font-size: 42px;
 `;
 
-const AirFlow = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 16x;
-  font-weight: 300;
-  color: #828282;
-  margin-bottom: 20px;
+const StyledSvg = styled.div`
   svg {
     width: 25px;
     height: auto;
@@ -67,17 +61,21 @@ const AirFlow = styled.div`
   }
 `;
 
-const Rain = styled.div`
+const AirFlow = styled(StyledSvg)`
   display: flex;
   align-items: center;
   font-size: 16x;
   font-weight: 300;
-  color: #828282;
-  svg {
-    width: 25px;
-    height: auto;
-    margin-right: 30px;
-  }
+  color: ${({ theme }) => theme.textColor};
+  margin-bottom: 20px;
+`;
+
+const Rain = styled(StyledSvg)`
+  display: flex;
+  align-items: center;
+  font-size: 16x;
+  font-weight: 300;
+  color: ${({ theme }) => theme.textColor};
 `;
 
 const Refresh = styled.div`
@@ -87,7 +85,7 @@ const Refresh = styled.div`
   font-size: 12px;
   display: inline-flex;
   align-items: flex-end;
-  color: #828282;
+  color: ${({ theme }) => theme.textColor};
   svg {
     margin-left: 10px;
     width: 15px;
@@ -100,29 +98,54 @@ const DayCloudyIcon = styled(DayCloudy)`
   flex-basis: 30%;
 `;
 
+const theme = {
+  light: {
+    backgroundColor: '#ededed',
+    foregroundColor: '#f9f9f9',
+    boxShadow: '0 1px 3px 0 #999999',
+    titleColor: '#212121',
+    temperatureColor: '#757575',
+    textColor: '#828282',
+  },
+  dark: {
+    backgroundColor: '#1F2022',
+    foregroundColor: '#121416',
+    boxShadow:
+      '0 1px 4px 0 rgba(12, 12, 13, 0.2), 0 0 0 1px rgba(0, 0, 0, 0.15)',
+    titleColor: '#f9f9fa',
+    temperatureColor: '#dddddd',
+    textColor: '#cccccc',
+  },
+};
+
 function App() {
+  const [currectTheme, setCurrentTheme] = useState('light');
   return (
-    <Container>
-      <WeatherCard>
-        <Location>台北市</Location>
-        <Description>多雲時晴</Description>
-        <CurrentWeather>
-          <Temperature>
-            23 <Celsius>C</Celsius>
-          </Temperature>
-          <DayCloudyIcon />
-        </CurrentWeather>
-        <AirFlow>
-          <AirFlowIcon />
-          23 m/h
-        </AirFlow>
-        <Rain>
-          <RainIcon />
-          48%
-        </Rain>
-        <Refresh>最後觀測時間：上午 12:03</Refresh>
-      </WeatherCard>
-    </Container>
+    <ThemeProvider theme={theme[currectTheme]}>
+      <Container>
+        <WeatherCard>
+          <Location>台北市</Location>
+          <Description>多雲時晴</Description>
+          <CurrentWeather>
+            <Temperature>
+              23 <Celsius>C</Celsius>
+            </Temperature>
+            <DayCloudyIcon />
+          </CurrentWeather>
+          <AirFlow>
+            <AirFlowIcon />
+            23 m/h
+          </AirFlow>
+          <Rain>
+            <RainIcon />
+            48%
+          </Rain>
+          <Refresh>
+            最後觀測時間：上午 12:03 <RefreshIcon />
+          </Refresh>
+        </WeatherCard>
+      </Container>
+    </ThemeProvider>
   );
 }
 
